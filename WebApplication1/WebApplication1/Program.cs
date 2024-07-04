fbf5e577561172764e16de7094d85eaa6e720c00
hgcha <ghkdthcha@naver.com>
2024-07-04 17:28:31 +0900

    /hello 엔드포인트 추가

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -24,4 +24,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.Map("/hello", app => {
+    app.Run(async context => {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
+
 app.Run();
